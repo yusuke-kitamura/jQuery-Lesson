24dc3aa0737bae58aaf2ee5e4c684f71d5c37936
北村　有佑 <yusukekitamura@kitamurayuuyuunoMacBook-Air.local>
2020-04-27 11:48:54 +0900

    cssファイル追加

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-$(document).ready(function () {
-    $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+	$('.box1').css({
+		'background-color' : '#0000FF',
+		'height' : '100px'
+	});
+
 });
